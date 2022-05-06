544218ded54f491d08ed96c7004dde7e77687fca
Lokirovskii <719220502@qq.com>
2022-05-06 15:59:42 +0800

    xyy: 用户管理界面

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/usermanager',
+    name: 'usermanager',
+    component: () => import(/* 用户管理界面 */'../views/UserManager.vue')
+  },
 ]
 
 const router = new VueRouter({
