4bc7afdf9090e4d189f6b4d1e57d093ab597c153
Lokirovskii <719220502@qq.com>
2022-05-16 22:56:21 +0800

    加了一个设为管理员的按钮

--- src/views/UserManager.vue
+++ src/views/UserManager.vue
@@ -7,9 +7,11 @@
       <el-table-column prop="registerdate" label="注册日期" width="150"></el-table-column>
       <el-table-column prop="exp" label="积分" width="130"></el-table-column>
       <el-table-column prop="userstatus" label="用户状态" width="150"></el-table-column>
-      <el-table-column label="操作" width="150">
+      <el-table-column label="操作" width="200">
         <template>
+          <!-- 此处应有各种v-if -->
           <el-button type="danger" size="small">禁言</el-button>
+          <el-button type="primary" size="small">设为管理员</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -25,27 +27,20 @@
         tableData: [{
           uid: '20020212',
           username: 'xyy',
-          email: '北京',
-          registerdate: '海淀区',
-          exp: 'buaa',
-          userstatus: 'dada'
+          email: '20373744@buaa.edu.cn',
+          registerdate: '20020212',
+          exp: '114514',
+          userstatus: 'dog'
         },
         {
-          uid: '20020212',
-          username: 'xyy',
-          email: '北京',
-          registerdate: '海淀区',
-          exp: 'buaa',
-          userstatus: 'dada'
+          uid: '1232456',
+          username: '???',
+          email: '百京',
+          registerdate: '西城区',
+          exp: 'infinity',
+          userstatus: 'unknown'
         },
-        {
-          uid: '20020212',
-          username: 'xyy',
-          email: '北京',
-          registerdate: '海淀区',
-          exp: 'buaa',
-          userstatus: 'dada'
-        }]
+        ]
       }
     }
   }
