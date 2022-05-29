0dd89c240a4726cf27afb38cc4f0e8a32e6db962
Lokirovskii <719220502@qq.com>
2022-05-29 12:16:42 +0800

    安装axios, 完成第一个交互

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="register">
     <h1>欢迎注册</h1>
-    <el-input placeholder="输入账号" v-model="input_id" clearable style = "width:300px"></el-input>
+    <el-input placeholder="输入账号" v-model="input_id" clearable style="width:300px"></el-input>
     <p>
-    <el-input placeholder="输入密码" v-model="input_password" clearable style = "width:300px" show-password></el-input>
+      <el-input placeholder="输入密码" v-model="input_password" clearable style="width:300px" show-password></el-input>
     <p>
-    <el-input placeholder="确认密码" v-model="input_password2" clearable style = "width:300px" show-password></el-input>
-    <div> <el-button v-on:click = "register" round>注册</el-button>
-    <el-button v-on:click = "goto_login" round>去登录</el-button> </div>
+      <el-input placeholder="确认密码" v-model="input_password2" clearable style="width:300px" show-password></el-input>
+    <div>
+      <el-button @click="register()" round>注册</el-button>
+      <router-link to="/login">
+        <el-button round>去登录</el-button>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+  import qs from "qs";
   export default {
-      data() {
+    data() {
       return {
         input_id: "",
         input_password: "",
@@ -21,18 +26,26 @@
       }
     },
     methods: {
-        goto_login(){
-        this.$router.replace('/login');
-        },
-        register(){
-            if (this.$data.input_password != this.$data.input_password2) alert('两次密码输入不一致！');
-            else
-            {
-                alert('注册成功！');
-                this.$router.replace('/login');
-            }
-            
+      register() {
+        if (this.$data.input_password != this.$data.input_password2) {
+          this.$message.error('两次输入的密码不一致！');
+          return;
         }
+        else {
+          this.$axios.post('/user/register', qs.stringify(this.$data))
+            .then(res => {
+              if (res.data.errno === 0) {
+                this.$message.success('注册成功！');
+              }
+              else {
+                this.$message.error(res.data.msg);
+              }
+            })
+            .catch(err => {
+              this.$message.error(err);
+            });
+        }
+      }
     }
-}
+  }
 </script>
