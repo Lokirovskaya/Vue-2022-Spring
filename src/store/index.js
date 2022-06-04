d179a94d13b1bd38e7a311b8866994fccda4defc
Lokirovskii <719220502@qq.com>
2022-06-04 14:13:23 +0800

    vuex持久化存储信息

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex)
 
@@ -33,5 +34,11 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [
+    createPersistedState({
+      storage: window.cookies,
+      key: 'store',
+    })
+  ]
 })
