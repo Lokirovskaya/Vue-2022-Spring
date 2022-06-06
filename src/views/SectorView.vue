19d85037c359bba2fd30bb456af47cb068dc696a
Lokirovskii <719220502@qq.com>
2022-06-06 18:38:23 +0800

    边框使用介绍

--- src/views/SectorView.vue
+++ src/views/SectorView.vue
@@ -1,74 +1,77 @@
 <template>
-  <el-container>
-    <el-header id="title">
-      这个板块的名称是 {{ sector_chinese_name[this.$route.query.name]}}
-    </el-header>
-    <el-header id="intro" v-html="sector_intro"></el-header>
-    <el-main>
+  <ForumBorder>
+    <el-container>
+      <el-header id="title">
+        这个板块的名称是 {{ sector_chinese_name[this.$route.query.name]}}
+      </el-header>
+      <el-header id="intro" v-html="sector_intro"></el-header>
+      <el-main>
 
-      <div v-if="this.$store.getters.is_logged">
-        <router-link :to="{path:'/sector/newpost', query:{name: this.$route.query.name}}">
-          <el-button type="primary">发新帖</el-button>
-        </router-link>
-      </div>
-      <div v-else>
-        <router-link to="/login">
-          <el-button type="primary">去登录才能发帖</el-button>
-        </router-link>
-      </div>
+        <div v-if="this.$store.getters.is_logged">
+          <router-link :to="{path:'/sector/newpost', query:{name: this.$route.query.name}}">
+            <el-button type="primary">发新帖</el-button>
+          </router-link>
+        </div>
+        <div v-else>
+          <router-link to="/login">
+            <el-button type="primary">去登录才能发帖</el-button>
+          </router-link>
+        </div>
 
-      <el-table :data="posting_data" stripe align="left">
+        <el-table :data="posting_data" stripe align="left">
 
-        <el-table-column min-width="5%"></el-table-column>
+          <el-table-column min-width="5%"></el-table-column>
 
-        <el-table-column label="标题" min-width="50%">
-          <template slot-scope="scope">
-            <router-link :to="{path:'post', query:{id:scope.row.posting_id}}">
-              <el-link id="art-title">
-                {{scope.row.posting_title}}
-              </el-link>
-            </router-link>
-            <div id="art-summary">TODO：摘要</div>
-          </template>
-        </el-table-column>
+          <el-table-column label="标题" min-width="50%">
+            <template slot-scope="scope">
+              <router-link :to="{path:'post', query:{id:scope.row.posting_id}}">
+                <el-link id="art-title">
+                  {{scope.row.posting_title}}
+                </el-link>
+              </router-link>
+              <div id="art-summary">TODO：摘要</div>
+            </template>
+          </el-table-column>
 
-        <el-table-column min-width="5%">
-          <template slot-scope="scope">
-            <img src="../assets/el-icon-like.png" style="width: 15px; height: 15px" />
-            {{scope.row.like_count}}
-          </template>
-        </el-table-column>
+          <el-table-column min-width="5%">
+            <template slot-scope="scope">
+              <img src="../assets/el-icon-like.png" style="width: 15px; height: 15px" />
+              {{scope.row.like_count}}
+            </template>
+          </el-table-column>
 
-        <el-table-column min-width="10%">
-          <template slot-scope="scope">
-            <img src="../assets/comment.png" style="width: 15px; height: 15px" />
-            {{scope.row.comment_count}}
-          </template>
-        </el-table-column>
+          <el-table-column min-width="10%">
+            <template slot-scope="scope">
+              <img src="../assets/comment.png" style="width: 15px; height: 15px" />
+              {{scope.row.comment_count}}
+            </template>
+          </el-table-column>
 
-        <el-table-column label="作者" min-width="15%">
-          <template slot-scope="scope">
-            <div id="art-author"><u>{{scope.row.username}}</u></div>
-          </template>
-        </el-table-column>
+          <el-table-column label="作者" min-width="15%">
+            <template slot-scope="scope">
+              <div id="art-author"><u>{{scope.row.username}}</u></div>
+            </template>
+          </el-table-column>
 
-        <el-table-column label="发表日期" min-width="15%">
-          <template slot-scope="scope">
-            <div id="art-date">{{scope.row.posting_time}}</div>
-          </template>
-        </el-table-column>
+          <el-table-column label="发表日期" min-width="15%">
+            <template slot-scope="scope">
+              <div id="art-date">{{scope.row.posting_time}}</div>
+            </template>
+          </el-table-column>
 
-      </el-table>
-    </el-main>
-  </el-container>
+        </el-table>
+      </el-main>
+    </el-container>
+  </ForumBorder>
 </template>
 
 
 <script>
   import qs from 'qs';
+  import ForumBorder from "@/components/ForumBorder";
   export default {
     name: 'SectorView',
-
+    components: { ForumBorder },
     data() {
       return {
         sector_intro: '',
