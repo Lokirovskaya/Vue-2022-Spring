0dad03dae3d14e6f9bcd1cc87f117639a956191a
Lokirovskii <719220502@qq.com>
2022-06-09 20:26:57 +0800

    回复版本

--- src/views/PersonCenter.vue
+++ src/views/PersonCenter.vue
@@ -4,25 +4,25 @@
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>用户中心</el-breadcrumb-item>
     </el-breadcrumb>
-
+    <div>
       <div v-if="this.$store.state.login_state == 0" class="login">
         <h1>请登录</h1>
 
-  </div>
-  <div>
-            <div v-if = "this.modify_state == 0">
-            <br/>
-            <el-descriptions title="用户信息" :column="3" :size="size" border style="position: relative;">
+      </div>
+      <div>
+
+        <div v-if="this.modify_state == 0">
+          <br />
+          <el-descriptions title="用户信息" :column="3" :size="size" border style="position: relative;">
             <template slot="extra">
-                    <div v-if="is_visit_self === 0">
-                    <el-button 
-                    style="position:relative;left:-10px;"
-                    size="small"
-                    @click="
-                      modify_state = 1;
-                      input_username = username;
-                      input_password = '';
-                      input_password2 = '';
+
+
+              <div v-if="is_visit_self === 0">
+                <el-button style="position:relative;left:-10px;" size="small" @click="                
+                    modify_state = 1;
+                    input_username = username;             
+                    input_password = '';
+                    input_password2 = '';
                     ">
                   修改信息
                 </el-button>
@@ -32,26 +32,21 @@
             <el-descriptions-item label="用户名">{{username}}</el-descriptions-item>
             <el-descriptions-item label="等级">lv{{level}}</el-descriptions-item>
             <el-descriptions-item label="经验值">{{exp_now}}</el-descriptions-item>
-        </el-descriptions>
-        <br/>
-            </div>
+          </el-descriptions>
+          <br />
+        </div>
 
         <div v-else>
           <br />
           <el-descriptions title="用户头像" :column="3" :size="size" border style="position: relative;">
 
-<el-upload
-  style="position: relative; left:-970px; top:20px"
-  class="avatar-uploader" action="" :http-request="upload_file" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload" :limit="1" :auto-upload="true">
-  <div style="position: relative;left:0px">
-  <img v-if="url_upload" :src="'http://43.138.55.69'+url_upload" class="avatar">
-  <img v_else :src="'http://43.138.55.69'+url_now" class="avatar">
-  </div>
-</el-upload>
-      <el-button size="small" style="position: relative;left:-20px;top:-28px;"
-      @click="goto_modify">
-      提交
-      </el-button>
+            <template slot="extra">
+
+              <!-- 上传并预览头像 -->
+              <!-- 支持jpg、jpeg、png、heic等 -->
+              <!-- <div class="alert-box-item" style="position: relative;left:-370px;top:15px;">  -->
+              <!-- <div class="bigImg-div" @click="toGetImg"> -->
+              <!-- <img class="bigImg" :src=url_upload v-if="url_upload"> -->
 
 
               <!-- <img class="bigImg" :src="url_upload" v-if="url_upload"> -->
@@ -61,19 +56,53 @@
               <!-- </div> -->
               <!-- </div> -->
 
-    <el-descriptions-item label="用户名">
-        <el-input placeholder="输入新用户名" v-model="input_username" clearable style="width:150px"></el-input>
-        <!-- <el-button size="small" style="position: relative;left:20px;" v-on:click="username = input_username" >修改</el-button> -->
-    </el-descriptions-item>
-</el-descriptions>
+              <el-upload style="position: relative; left:-970px; top:20px" class="avatar-uploader" action=""
+                :http-request="upload_file" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload"
+                :limit="1" :auto-upload="true">
+                <div style="position: relative;left:0px">
+                  <img v-if="url_upload" :src="'http://43.138.55.69'+url_upload" class="avatar">
+                  <img v_else :src="'http://43.138.55.69'+url_now" class="avatar">
+                  <!-- <div v-else style="position: relative;top:19px;">上传头像</div> -->
+                  <!-- <i v-else class="el-icon-plus avatar-uploader-icon"></i> -->
+                </div>
+              </el-upload>
 
-            <!-- <el-descriptions-item label="学校"> -->
-            <!-- <el-input placeholder="输入新学校" v-model="input_school" clearable style="width:250px"></el-input> -->
-            <!-- <el-button size="small" style="position: relative;left:20px;" v-on:click="school = input_school">修改</el-button> -->
-            <!-- </el-descriptions-item> -->
+              <!-- <el-button size="small" style="position: relative;left:-1070px;top:-25px;">上传头像</el-button> -->
+              <!-- <el-button size="small"><router-link to="/viewifo">修改完成</router-link></el-button> -->
 
-<div v-if="this.modify_state == 0">
-        <el-menu mode="horizontal">
+              <el-button size="small" style="position: relative;left:-20px;top:-28px;" @click="goto_modify">
+                提交
+              </el-button>
+
+
+            </template>
+
+            <el-descriptions-item label="用户名">
+              <el-input placeholder="输入新用户名" v-model="input_username" clearable style="width:150px"></el-input>
+            </el-descriptions-item>
+
+
+          </el-descriptions>
+
+          <br />
+          <div style="position: relative;left:-330px;">
+            修改密码：
+            <el-input placeholder="输入新密码" v-model="input_password" clearable
+              style="width:150px; position: relative;left:0px;" show-password></el-input>
+            <!-- <br/><br/> -->
+            <el-input placeholder="确认密码" v-model="input_password2" clearable
+              style="width:150px; position: relative;left:20px;" show-password></el-input>
+            <el-button size="small" style="position: relative;left:40px;" @click="modify_pwd">修改密码</el-button>
+          </div>
+          <br />
+        </div>
+
+        <!-- <el-menu-item index="/PersonCenter" style="position: relative;left:950px;"><el-link><router-link to="/modifyifo">修改个人资料</router-link></el-link></el-menu-item> -->
+        <!-- <el-link :underline="false" style="position: relative;right:2px;bottom:-19px" @click="goto_login">编辑资料</el-link> -->
+
+
+        <div v-if="this.modify_state == 0">
+          <el-menu mode="horizontal">
 
             <el-collapse v-model="activeNames" @change="handleChange">
 
@@ -82,11 +111,13 @@
                 <el-table :data="posting_data" stripe="true" align="left">
                   <el-table-column label="标题" min-width="70%">
 
-              <template slot-scope="scope">
-                <router-link style="color:black" :to="{path:'/post', query:{id:scope.row.posting_id}}">
-                <div class="art-title"><el-link>{{scope.row.title}}</el-link></div>
-                </router-link>
-              </template>
+                    <template slot-scope="scope">
+                      <router-link style="color:black" :to="{path:'/post', query:{id:scope.row.posting_id}}">
+                        <div class="art-title">
+                          <el-link>{{scope.row.title}}</el-link>
+                        </div>
+                      </router-link>
+                    </template>
 
                   </el-table-column>
 
@@ -122,12 +153,10 @@
                       <div class="art-date">{{scope.row.time}}</div>
                     </template>
                   </el-table-column>
+                </el-table>
+              </el-collapse-item>
 
-          </el-table>
-
-            </el-collapse-item>
-
-            <el-collapse-item title="近期评论" name="3">
+              <el-collapse-item title="近期评论" name="3">
 
                 <el-table :data="reply_data" stripe="true" align="left">
 
@@ -143,43 +172,49 @@
 
                   </el-table-column>
 
-          <el-table-column label="获赞" min-width="8%">
-            <template slot-scope="scope">
-              <img src="../assets/el-icon-like.png" style="width: 15px; height: 15px" />
-              {{scope.row.like_count}}
-            </template>
-          </el-table-column>
+                  <el-table-column label="获赞" min-width="8%">
+                    <template slot-scope="scope">
+                      <img src="../assets/el-icon-like.png" style="width: 15px; height: 15px" />
+                      {{scope.row.like_count}}
+                    </template>
+                  </el-table-column>
 
-          <el-table-column label="评论时间" min-width="18%">
-            <template slot-scope="scope">
-              <div class="art-date">{{scope.row.time}}</div>
-            </template>
-          </el-table-column>
+                  <el-table-column label="评论时间" min-width="18%">
+                    <template slot-scope="scope">
+                      <div class="art-date">{{scope.row.time}}</div>
+                    </template>
+                  </el-table-column>
 
                 </el-table>
               </el-collapse-item>
 
 
             </el-collapse>
-            
-        </el-menu>
-</div>
-  </div>
+
+          </el-menu>
+        </div>
+        <!-- <el-button @click="test1()" round>测试</el-button> -->
+
+      </div>
+
     </div>
 
   </ForumBorder>
 </template>
 
 <script>
-import ForumBorder from "@/components/ForumBorder";
-
-import qs from "qs";
-export default {
-  components: { ForumBorder },
-  name: 'PersonCenter',
-
-data(){
-    return{
+  import ForumBorder from "@/components/ForumBorder";
+  // import PostHead from "@/components/PostHead";
+  // import PostReply from "@/components/PostReply";
+  // let inputElement = null
+  import qs from "qs";
+  export default {
+    components: { ForumBorder },
+    name: 'PersonCenter',
+    //   components:{PostHead,PostReply},
+    // components:{PostHead},
+    data() {
+      return {
         articles1: [
           {
             id: 1,
@@ -200,60 +235,63 @@ data(){
             date: '2002-02-12',
           },
         ],
-        modify_state:0,
-        username:this.$store.state.username,
+        modify_state: 0,
+        username: this.$store.state.username,
         exp_now: undefined, //当前经验值
         exp_next_lv: undefined, //下一等级经验值
         level: undefined,
-
-        input_username:undefined,
-        input_password:undefined,
-        input_password2:undefined,
-        is_visit_self:0, //0：访问自己主页
-
+        input_username: undefined,
+        input_password: undefined,
+        input_password2: undefined,
+        is_visit_self: 0, //0：访问自己主页
         url_upload: undefined,
-        url_now:undefined,
-
+        url_now: undefined,
         posting_data: [],
         reply_data: [],
-    }
-},
-methods:
-{
-      getSimpleText(html){
-      var re1 = new RegExp("<.+?>","g");//匹配html标签的正则表达式，"g"是搜索匹配多个符合的内容
-      var msg = html.replace(re1,'');//执行替换成空字符
-      return msg;
+      }
+    },
+    methods:
+    {
+      // test1(){
+      //   alert('1234');
+      // },
+      // goto_view_ifo(){
+      //     this.$router.replace('/viewifo');
+      // },
+      getSimpleText(html) {
+        var re1 = new RegExp("<.+?>", "g");//匹配html标签的正则表达式，"g"是搜索匹配多个符合的内容
+        var msg = html.replace(re1, '');//执行替换成空字符
+        return msg;
       },
-
-    goto_modify(){
-    let user_ifo = {
-        username:this.input_username,
-      };
-    this.$axios.post('/user/modify_username', qs.stringify(user_ifo),{
-        headers: {
-          username: this.$store.state.username,
-          token: this.$store.state.token,
-        }
-      })
-            .then(res => {
-              if (res.data.errno === 0) {
-                this.username = this.input_username; //更新页面变量
-                this.$store.state.username = this.username; //更新全局变量
-                // this.$store.token = res.data.token; //更新token
-                this.$message.success(res.data.msg);
-                this.modify_state = 0;
-              }
-              else {
-                this.$message.error(res.data.msg);
-              }
-            })
-            .catch(err => { 
-              this.$message.error(err);
-            });
-
+      goto_modify() {
+        let user_ifo = {
+          username: this.input_username,
+          // reply_id:this.reply_id,
+          // photo:this.url_upload
+        };//数据打包
+        // console.log(user_ifo);
+        this.$axios.post('/user/modify_username', qs.stringify(user_ifo), {
+          headers: {
+            username: this.$store.state.username,
+            token: this.$store.state.token,
+          }
+        })
+          .then(res => {
+            if (res.data.errno === 0) {
+              this.username = this.input_username; //更新页面变量
+              this.$store.state.username = this.username; //更新全局变量
+              // this.$store.token = res.data.token; //更新token
+              this.$message.success(res.data.msg);
+              this.modify_state = 0;
+            }
+            else {
+              this.$message.error(res.data.msg);
+            }
+          })
+          .catch(err => {
+            this.$message.error(err);
+          });
       },
-
       init_view() {
         console.log('run init_view');
         console.log(this.$store.state.username);
@@ -287,7 +325,6 @@ methods:
             });
           // console.log(this.url_now);
         }
-
         else //访问自己的主页
         {
           this.is_visit_self = 0;
@@ -321,7 +358,6 @@ methods:
         }
         console.log('print:' + this.url_upload);
       },
-
       modify_pwd() {
         let password_ifo = {
           password_1: this.input_password,
@@ -333,6 +369,9 @@ methods:
         else if (this.input_password2 === '') {
           this.$message.error('请再次输入密码！');
         }
+        // else if (this.input_password !== this.input_password2) {
+        //   this.$message.error('两次输入的密码不一致！');
+        // }
         else {
           // alert('success');
           this.$axios.post('/user/modify_password', qs.stringify(password_ifo),
@@ -355,13 +394,12 @@ methods:
               this.$message.error(err);
             });
         }
-    },
-    
-    upload_file(e) {
+      },
+
+      upload_file(e) {
 
         let formData = new FormData();
         formData.append('in_file', e.file);
-
         let my_axios = this.$axios.create({
           withCredentials: true,
           headers: {
@@ -370,7 +408,6 @@ methods:
             'Content-Type': 'multipart/form-data'
           }
         });
-
         my_axios.post('/user/modify_photo', { photo: e.file })
           .then(res => {
             if (res.data.errno === 0) {
@@ -378,6 +415,8 @@ methods:
               this.url_now = res.data.photo;
               this.url_upload = res.data.photo;
               this.$store.state.user_photo = res.data.photo;
+              // this.file_id = res.data.file_id;
+              // this.url_upload = res.data.url;
             }
             else {
               this.$message.error(res.data.msg);
@@ -397,13 +436,13 @@ methods:
         console.log('before');
         const isJPG = file.type === 'image/jpeg';
         const isLt2M = file.size / 1024 / 1024 < 2;
-
         if (!isJPG) {
           this.$message.error('上传头像图片只能是 JPG 格式!');
         }
         if (!isLt2M) {
           this.$message.error('上传头像图片大小不能超过 2MB!');
         }
+
         return isJPG && isLt2M;
       }
 
